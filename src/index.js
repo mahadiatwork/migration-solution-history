2351fb6a832913d287149d06f666b753015c2acd
mahadiinsta <mahadi@instawebworks.com.au>
2025-02-22 12:24:37 +0600

    Revert "add: notification."
    This reverts commit a0ef5144ef418d646d200fc4526b1ec80d35a54b.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
-import { SnackbarProvider } from "notistack";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SnackbarProvider
-      iconVariant={{
-        error: "",
-      }}
-    >
-      <CssBaseline />
-      <App />
-    </SnackbarProvider>
+    <CssBaseline />
+    <App />
   </React.StrictMode>
 );
